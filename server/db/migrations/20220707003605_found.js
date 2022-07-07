e25368b7a78062152f260311b1015957da2fd19a
Emma Sutherland <103154693+emma-sutherland@users.noreply.github.com>
2022-07-07 12:40:53 +1200

    created Found table

---
+++ server/db/migrations/20220707003605_found.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('found', (table) => {
+    table.increments('id').primary()
+    table.string('species')
+    table.string('photo')
+    table.integer('user_id')
+  })
+  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('found')
+  
+};
