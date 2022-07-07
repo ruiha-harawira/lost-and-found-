712d3c6e37f56fc93bd31863b0bddcbcbf56477d
Hannah Beattie <hannahdbeattie@gmail.com>
2022-07-08 10:57:42 +1200

    fixed my spelling error

--- client/components/LostPetSelector.jsx
+++ client/components/LostPetSelector.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux'
 
 // HOOK
 
-// this hook is how the fron end accessess data in the back end
+// this hook is how the front end accessess data in the back end
 //(communicating with the THUNK middleware to retreive and post data to/from the db)
 
 export const useLostPets = () => useSelector(selectLostPets)
