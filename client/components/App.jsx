d319b742223fece43f98fe3b5fa5d342f79f38c2
Emma Sutherland <103154693+emma-sutherland@users.noreply.github.com>
2022-07-08 14:39:58 +1200

    finished found form

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,14 +2,16 @@ import React, { useEffect } from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 
-import FoundPets from './foundPets'
-import LostPets from './lostPets'
-import LostForm from './lostForm'
-import FoundForm from './foundForm'
+import FoundPets from './FoundPets'
+import LostPets from './LostPets'
+import LostForm from './LostForm'
+import FoundForm from './FoundForm'
 import Login from './Login'
 import Register from './Register'
 import Nav from './Nav'
 import Home from './Home'
+
+
 
 
 import { checkAuth } from '../Redux/authActions'
@@ -40,14 +42,14 @@ function App() {
             path="/"
             element={auth.isAuthenticated ? <Home /> : <Login />}
           />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path='/lostForm' element={<LostForm />} />
-          <Route path='/foundForm' element={<FoundForm />} />
-          <Route path='/lostPets' element={<LostPets />} />
-          <Route path='/foundPets' element={<FoundPets />} />
-        </Routes>
-        <FoundForm/>
+          <Route path="./Login" element={<Login />} />
+          <Route path="./Register" element={<Register />} />
+          <Route path='./LostForm' element={<LostForm />} />
+          <Route path='./FoundForm' element={<FoundForm />} />
+          <Route path='./LostPets' element={<LostPets />} /> 
+          <Route path='./FoundPets' element={<FoundPets />} />
+        </Routes> 
+  
 
         
       </div>
