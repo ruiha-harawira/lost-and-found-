caf40a60d283f592bdd1bdf546f8a94df56ae20e
Kais <kais.lutfi1@gmail.com>
2022-07-08 12:18:34 +1200

    fix conflict

--- client/components/LostPet.jsx
+++ client/components/LostPet.jsx
@@ -5,7 +5,7 @@ export const LostPet = (props) => {
     <div className='onePetCard'>
       <h3>{props.name}</h3>
       <p>{props.species}</p>
-      <img src={props.photo} alt={`Lost pet who goes by 60 ${props.name}`}/>
+      <img src={props.photo} alt={`Lost pet who goes by  ${props.name}`}/>
     </div>
   )
 }
